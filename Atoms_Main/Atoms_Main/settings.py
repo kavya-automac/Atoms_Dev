e03cc0e7d5d6a5447ff3364e25e9838a67e2d0f2
kavya <setavakavya3@gmail.com>
2024-01-24 11:21:47 +0530

    completed drowpdown module

--- Atoms_Main/Atoms_Main/settings.py
+++ Atoms_Main/Atoms_Main/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.29.144','127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
--- Atoms_Main/Atoms_users/urls.py
+++ Atoms_Main/Atoms_users/urls.py
@@ -1,10 +1,8 @@
-
 from django.urls import path
 from .views import *
 
 urlpatterns = [
-
-    # path('get_node_LR_api/', get_node_LR_api),
+    path('Machine_module/', Machine_module),
 
 ]
 
