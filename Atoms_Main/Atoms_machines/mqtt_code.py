09fa65cb03dc87225496ba8fb2da01e806b8dfca
kavya <setavakavya3@gmail.com>
2024-09-17 15:49:12 +0530

    host,port settedup in mqtt_code file

--- Atoms_Main/Atoms_machines/mqtt_code.py
+++ Atoms_Main/Atoms_machines/mqtt_code.py
@@ -71,10 +71,15 @@ client.on_connect = on_connect
 client.on_message = on_message
 # client.on_disconnect = on_disconnect
 # client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
+# client.connect(
+#    host=settings.MQTT_SERVER,
+#    port=settings.MQTT_PORT,
+#    keepalive=settings.MQTT_KEEPALIVE
+# )
 client.connect(
-   host=settings.MQTT_SERVER,
-   port=settings.MQTT_PORT,
-   keepalive=settings.MQTT_KEEPALIVE
+   host='broker.hivemq.com',
+   port=1883,
+   keepalive=60
 )
 
 # client.loop_start()
