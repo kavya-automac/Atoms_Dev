125f2e17cb1f299a21b6669d6ab128439be308ff
kavya <setavakavya3@gmail.com>
2024-09-17 15:38:13 +0530

    hive mqtt username pass commented

--- Atoms_Main/Atoms_machines/mqtt_code.py
+++ Atoms_Main/Atoms_machines/mqtt_code.py
@@ -70,7 +70,7 @@ client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
 client.on_message = on_message
 # client.on_disconnect = on_disconnect
-client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
+# client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
 client.connect(
    host=settings.MQTT_SERVER,
    port=settings.MQTT_PORT,
